8205fb1de9194bd04a53f560d0861ba13d814beb
Roshan Kumar <roshankunaguero09@gmail.com>
2019-09-29 03:39:31 +0530

    heroku deployment mongo db error fixing

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
     module.exports = {
-        mongoURI: "mongodb+srv://roshan:9939105936@music-app-db-hexhh.mongodb.net/vidjot-prod?retryWrites=true&w=majority"};
+        mongoURI: "mongodb+srv://roshan:9939105936@music-app-db-hexhh.mongodb.net/vidjot-prod"};
     } else {
         module.exports = {
             mongoURI: 'mongodb://localhost/vidjot-dev'
